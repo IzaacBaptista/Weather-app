5511cc788df79ef9a6d6e167243cee8e45f860ba
izaac baptista <izaacbaptista@gmail.com>
2024-12-04 21:27:13 -0300

    feat(deploy): deploy aws and vercel

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: "https://on5taoyhke.execute-api.us-east-1.amazonaws.com/dev",
 });
 
 export default api;
